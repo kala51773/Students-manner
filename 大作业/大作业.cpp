270a2960eae49e7d86c4ab1e2d9913d2cf4e7162
kala51773 <48636164+kala51773@users.noreply.github.com>
2020-12-02 07:16:51 +0800

    Update 大作业.cpp

--- 大作业/大作业.cpp
+++ 大作业/大作业.cpp
@@ -1,258 +1,489 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-struct student
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<cstring >
+#include<stdio.h>
+#include<time.h>
+#include<fstream>
+using namespace std;
+typedef class  STUDENT
 {
+public:
+	STUDENT()
+	{
+		num=0;
+		name[20]=0;
+		sex[100]=0;
+		major[100]=0;
+		address[100]=0;
+		englishscore=0;
+		year=0;
+		month=0;
+		date=0;
+	}
+	~STUDENT()
+	{
+		
+	}
+	void addstudent(STUDENT* p);
+	void printstudent(STUDENT* p);
+	void deletestudent(STUDENT* p);
+	void search(STUDENT* p);
+	void statistical(STUDENT* p);
+	void rank(STUDENT* p);
+	void storestudent(STUDENT* p);
+	int Repeat(int n, STUDENT* p1);
+	void readstudent();
+	void modifystudent(STUDENT* p);
+	STUDENT* next;
+private:
+	int num;
 	char name[20];
-	int id;
-	char sex[10];
-	char tel[20];
-};
-struct linknode {
-	student date;
-	linknode* next;
-
-};
-
-//创建结构体表示节点
-void menu();//打印交互菜单
-void keyDown();//实现交互
-void printkinklist(linknode* p);//打印整个链表
-void insectlinklist(linknode* head, student date);//头插法插入
-void deletelinknode(linknode* p, char* name);
-linknode* ncreatlist();//创建链表
-linknode* creatnode(student date);//创建链表节点
-linknode* seachdatenode(linknode* p, char* name);
-linknode* list;
-void read(linknode* head, char* file);//读取操作
-void write(linknode* head, char* file);
-/*主函数开始*/
-
-
+	char sex[100];
+	char major[100];
+	char address[100];
+	int  englishscore;
+	int year;
+	int month;
+	int date;
+}stu;
+stu* createlist()//创链表 
+{
+	stu* p;
+	p = (stu*)malloc(sizeof(stu));
+	p->next = NULL;
+	return p;
+}
 int main()
 {
-	list = ncreatlist();
-	list->next = NULL;
-	char a[20] = "ass.txt";
-	read(list, a);
-	while (1)
+	system("color E");
+	printf("\n\n\n\n\n\n\n\t\t\t\t\t本地账号登录\n");
+	printf("\n\t\t\t\t请输入密码：\n");
+	printf("\t\t\t\t");
+	char password[20];
+	int i;
+	for (i = 0;i < 3;i++)
 	{
-		menu();
-		keyDown();
-		write(list, a);
-		getchar();
-
-	}
-	return 0;
-}
-
-
-/*主函数结束*/
-/*读写操作函数*/
-
-void read(linknode* head, char* file)
-{
-	//打开
-	FILE* fp;
-	student date;
-	fp = fopen(file, "r");
-
-	if (fp == NULL)
-	{
-
-		fp = fopen(file, "w+");//不存在文件就创建文件
-		printf("不存在文件");
-	}
-	//读文件
-	while (fscanf(fp, "%s\t%d\t%s\t%s\n", &date.name, &date.id, &date.sex, &date.tel) != EOF)
-	{
-		insectlinklist(head, date);
-	}
-	//关闭文件
-	fclose(fp);
-}
-void write(linknode* head, char* file)
-{
-	FILE* fp;
-	fp = fopen(file, "w");
-	linknode* current = head->next;
-	while (current)
-	{
-		fprintf(fp, "%s\t%d\t%s\t%s\n", current->date.name, current->date.id, current->date.sex, current->date.tel);
-		current = current->next;
-	}
-	fclose(fp);
-}
-
-void menu()
-{
-	printf("***********************************\n");
-	printf("**       【学生信息管理系统】    **\n");
-	printf("***********************************\n");
-	printf("**                               **\n");
-	printf("**      1.-----录入信息----      **\n");
-	printf("**      2.-----浏览信息----      **\n");
-	printf("**      3.-----修改信息----      **\n");
-	printf("**      4.-----删除信息----      **\n");
-	printf("**      5.-----查找信息----      **\n");
-	printf("**      6.-----退出程序----      **\n");
-	printf("**                               **\n");
-	printf("***********************************\n");
-
-}
-void keyDown()
-{
-	int x;
-	scanf("%d", &x);
-	student date;
-	switch (x)
-	{
-	case 1:printf("**-----------录入信息-----------**\n");
-		printf("请输入学生姓名、学号、性别、电话\n");
-		fflush(stdin);//清空缓存区 
-		scanf("%s %d %s %s", date.name, &date.id, date.sex, date.tel);
-		insectlinklist(list, date);
-		printf("录入成功\n");
-		break;
-
-	case 2:printf("**-----------浏览信息-----------**\n");
-		system("cls");
-		printkinklist(list);
-		getchar();
-		break;
-	case 3:printf("**-----------修改信息-----------**\n");
-		char ch[20];
-		printf("请输入人员姓名\n");
-		scanf("%s", ch);
-		linknode* tmpx;
-		tmpx = seachdatenode(list, ch);
-		if (tmpx != NULL)
+		scanf("%s", password);
+		if (strcmp(password, "000000") == 0)
 		{
-			printf("该人员信息：\n姓名\t学号\t性别\t电话\n");
-			printf("%s\t%d\t%s\t%s\t\n", tmpx->date.name, tmpx->date.id, tmpx->date.sex, tmpx->date.tel);
-
-			printf("请输入修改后信息\n");
-			scanf("%s %d %s %s", tmpx->date.name, &tmpx->date.id, tmpx->date.sex, tmpx->date.tel);
+			printf("登陆成功！！！\n");
+			system("pause");
+			break;
 		}
 		else
-			printf("查无此人\n");
-		break;
-
-
-		break;
-	case 4:printf("**-----------删除信息-----------**\n");
-		printf("请输入要删除的学生姓名\n");
-		char tmp[20];
-		scanf("%s", tmp);
-		deletelinknode(list, tmp);
-
-		break;
-	case 5:printf("**-----------查找信息-----------**\n");
-		char tmp1[20];
-		scanf("%s", tmp1);
-		linknode* tmp2;
-		tmp2 = seachdatenode(list, tmp1);
-		if (tmp2 != NULL)
 		{
-			printf("姓名\t学号\t性别\t电话\n");
-			printf("%s\t%d\t%s\t%s\t\n", tmp2->date.name, tmp2->date.id, tmp2->date.sex, tmp2->date.tel);
+			printf("密码输入错误，请重新输入：\n");
+			printf("你还有%d次机会！！！\n", 2 - i);
 		}
-		else
-			printf("查无此人\n");
-		break;
-	case 6:printf("**-----------查找信息-----------**\n");
-		printf("欢迎下次使用");
+	}
+	if (i == 3)
+	{
+		printf("对不起，你已经失败，退出登录系统！！！\n");
+		system("pause");
 		exit(0);
-		break;
-	default:break;
 	}
-
-
-
-}
-linknode* ncreatlist()
-{
-	linknode* head = (linknode*)malloc(sizeof(linknode));
-	head->next = NULL;
-	return head;
-}
-
-linknode* creatnode(student date)
-{
-	linknode* current = (linknode*)malloc(sizeof(linknode));
-	current->date = date;
-	current->next = NULL;
-	return current;
-
-}
-
-void printkinklist(linknode* p)
-{
-
-	linknode* pMove = p->next;
-	printf("姓名\t学号\t性别\t电话\n");
-	if (pMove == NULL)
-		return;
+	system("cls");
+	stu t;
+	stu* pstu = NULL;
+	pstu = createlist();
 	while (1)
 	{
-		if (pMove->next != NULL)
+		system("cls");
+		cout << "==============================================欢迎使用学生信息管理系统==================================================\n";
+		cout << "\t	          			======请选择功能列表======\n";
+		cout << "\t	 		 		    录入学生信息请按1\n";
+		cout << "\t	 		 		    显示学生信息请按2\n";
+		cout << "\t	 		 		    删除学生信息请按3\n";
+		cout << "\t	 		 		    查询学生信息请按4\n";
+		cout << "\t	 		 		    统计学生信息请按5\n";
+		cout << "\t	 		 	 	    按照英语成绩排序请按6\n";
+		cout << "\t	 		 		    修改学生信息请按7\n"; 
+		cout << "\t	 		 		    储存信息请按8\n";
+		cout << "\t	 		 		    读取文件请按9\n";
+		cout << "\t	 		 		    0.退出系统\n";
+		cout << "========================================================================================================================\n";
+		system("date/t");
+		system("time/t");
+		char ch = getch();
+		switch (ch)
 		{
-			printf("%s\t %d\t %s\t %s\n", pMove->date.name, pMove->date.id, pMove->date.sex, pMove->date.tel);
-			pMove = pMove->next;
-		}
-		else
-		{
-			printf("%s\t %d\t %s\t %s\n", pMove->date.name, pMove->date.id, pMove->date.sex, pMove->date.tel);
+		case '1'://录入信息
+			system("CLS");
+			t.addstudent(pstu);
+			break;
+		case '2':
+			system("CLS");
+			t.printstudent(pstu);
+			break;//打印学生信息
+		case '3':
+			system("CLS");
+			t.deletestudent(pstu);
+			break;//保存学生信息
+		case '4':
+			system("CLS");
+			t.search(pstu);
+			break;//读取学生信息 
+		case '5':
+			system("CLS");
+			t.statistical(pstu);
+			break;
+		case '6':
+			system("CLS");
+			t.rank(pstu);
+			break;
+		case '7':
+			system("CLS");
+			t.modifystudent(pstu);
+			break;
+		case '8':
+			system("CLS");
+			t.storestudent(pstu);
+			break;
+		case '9':
+			t.readstudent();
+			break;
+		case '0':
+			system("CLS");
+			cout << "欢迎再次使用!\n";
+			return 0;
+			break;//退出程序
+		default:
+			cout << "		                		输入有误，请重新输入\n";
+			system("pause");
 			break;
 		}
 	}
-
 }
-
-void insectlinklist(linknode* head, student date)
+void stu::addstudent(stu* p)//增加学生信息 
 {
-	linknode* current = creatnode(date);
-	current->next = head->next;
-	head->next = current;
-
-}
-
-void deletelinknode(linknode* p, char* name)
-{
-	linknode* pre = p;
-	linknode* current = p->next;
-	if (current == NULL)
+	int i, n, t;
+	stu* pnew;
+	pnew=p->next;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+	cout << "请输入学生的信息\n";
+	cout << "请输入添加学生的数量\n";
+	cin >> n;
+	for (i = 0;i < n;i++)
 	{
-		printf("该节点为尾节点\n");
-		return;
-	}
-
-	while (strcmp(current->date.name, name))
-	{
-		current = current->next;
-		pre = pre->next;
-		if (current == NULL)
+		
+		pnew = new stu;
+		cout << "学号:";
+		cin >> t;
+		t = Repeat(t, p);
+		if (t == 0)
 		{
-			printf("未找到该位置\n");
-			return;
+			break;
+		}
+		pnew->num = t;
+		cout << "姓名:";
+		cin >> pnew->name;
+		cout << "性别:";
+		cin >> pnew->sex;
+		cout << "专业:";
+		cin >> pnew->major;
+		cout << "出生日期:" << endl;
+		cout << "年:";
+		cin >> pnew->year;
+		cout << "月:";
+		cin >> pnew->month;
+		cout << "日:";
+		cin >> pnew->date;
+		cout << "地址:";
+		cin >> pnew->address;
+		cout << "英语成绩:";
+		cin >> pnew->englishscore;
+		pnew->next = p->next;
+		p->next = pnew;
+		cout << "添加成功\n";
+	}
+	system("pause");
+}
+void stu::printstudent(stu* p)//显示学生信息 
+{
+	p = p->next;
+	while (p != NULL)
+	{
+		cout << "学号:" << p->num << "\t";
+		cout << "姓名:" << p->name << "\t";
+		cout << "性别:" << p->sex << "\t";
+		cout << "专业:" << p->major << "\t";
+		cout << "出生日期:" << p->year << "." << p->month << "." << p->date << "\t";
+		cout << "家庭地址:" << p->address << "\t";
+		cout << "英语成绩:" << p->englishscore << endl;
+		p = p->next;
+	}
+	system("pause");
+}
+//查重 
+int stu::Repeat(int n, stu* p1)//出现重复学号无法输入 
+{
+R:stu* p = p1->next;
+	while (p != NULL)
+	{
+		if (p->num == n)
+		{
+			cout << "此学号已有 请重新输入！" << endl;
+			cout << "请输入学号";
+			cin >> n;
+			goto R;
+		}
+		p = p->next;
+	}
+	return n;
+}
+void stu::deletestudent(stu* p)//删除学生信息 
+{
+	stu* temp;
+	stu*p1=p;
+	temp = p->next;
+	string name;
+	cout << "输入要删除的姓名";
+	cin >> name;
+	while (temp != NULL)
+	{
+		if (temp->name == name)
+		{
+			p1->next = temp->next;
+		}
+		temp = temp->next;
+		p1 = p1->next;
+	}
+	cout << "删除成功" << endl;
+	system("pause");
+}
+void stu::search(stu* p)//查找学生信息 
+{
+	string name;
+	cout << "请输入查找人的姓名\n";
+	cin >> name;
+	if (p->next == NULL)
+	{
+		cout << "抱歉查无此人" << endl;
+	}
+	while (p != NULL)
+	{
+		if (name == p->name)
+		{
+			cout << endl;
+			cout << "                               查找人的信息\n";
+			cout << "学号:" << p->num << "\t" << "姓名:" << p->name << "\t" << "性别:" << p->sex << "\t" << "专业:" << p->major << "\t" << "出生日期:" << p->year << "年" << p->month << "月" << p->date << "日" << "\t" << "家庭地址:" << p->address << "\t" << "英语成绩:" << p->englishscore << endl;
+		}
+		p = p->next;
+	}
+	system("pause");
+}
+void stu::statistical(stu* p)//统计学生信息数量 
+{
+	stu* p1 = p->next;
+	string a = "专业", b = "性别", c = "年龄";
+	string type;
+	cout << "请输入统计的类型" << endl;
+	cin >> type;
+	if (type == a)//统计专业数量 
+	{
+		char search[100];
+		cout << "请输入专业名:";
+		cin >> search;
+		int sum = 0;
+		if (p1 == NULL)
+		{
+			cout << "无学生信息!";
+		}
+		else
+		{
+			while (p1 != NULL)
+			{
+				if (!strcmp(p1->major, search))
+				{
+					sum++;
+				}
+				p1 = p1->next;
+			}
+		}
+		cout << search << "\n" << "总人数：" << sum << endl;
+	}
+	if (type == b)//统计性别数量 
+	{
+		int sum = 0;
+		char sex[100];
+		cout << "请输入性别:";
+		cin >> sex;
+		if (p1 == NULL)
+		{
+			cout << "无学生信息" << endl;
+		}
+		while (p1 != NULL)
+		{
+			if (!strcmp(p1->sex, sex))
+			{
+				sum++;
+			}
+			p1 = p1->next;
+		}
+		cout << sex << "\n" << "总人数:" << sum << endl;
+	}
+	else if (type == c)                                          //按年龄统计 
+	{
+		int age,age1;
+		int sum=0;
+		cout << "请输入您要统计的年龄：\n";
+		cin >> age;
+		if (p1 == NULL)
+		{
+			cout << "无学生信息！\n";
+		}
+		else
+		{
+			while(p1!=NULL)
+			{
+				age1=2020-p1->year;
+				if(age==age1)
+				{
+					sum++;
+				}
+				p1=p1->next;
+			}
+			cout<<"总人数:"<<sum<<endl;
 		}
 	}
-	pre->next = current->next;
-	free(current);
-	printf("删除成功！\n");
+	system("pause");
 }
-
-linknode* seachdatenode(linknode* p, char* name)
+void stu::rank(stu* p)//按照英语成绩排序 
 {
-	linknode* current = p->next;
-	if (current == NULL)
-		return NULL;
-	while (current != NULL && strcmp(current->date.name, name))
+	stu* p1 = new stu;
+	stu* p2 = new stu;
+	stu  p3;
+	p1 = p->next;
+	while (p1 != NULL)
 	{
-		current = current->next;
+		p2 = p1->next;
+		while (p2 != NULL)
+		{
+			if (p1->englishscore > p2->englishscore)
+			{
+				p3 = *p1;//指针域，数据域交换 
+				*p1 = *p2;
+				*p2 = p3;
+				p3.next = p1->next;//指针域交换 
+				p1->next = p2->next;
+				p2->next = p3.next;
+			}
+			p2 = p2->next;
+		}
+		p1 = p1->next;
 	}
-	return current;
-
+	cout << "\t\t\t\t\t入门英语成绩从低到高排序已完成" << endl;
+	system("pause");
 }
+void stu::modifystudent(stu* p)//修改学生信息 
+{
+	int n;
+	cout << "请输入要修改学生的学号" << endl;
+	cin >> n;
+	stu* p1 = new stu;
+	p1 = p->next;
+	if (p1 == NULL)
+	{
+		cout << "抱歉,查无此人" << endl;
+	}
+	while (p1 != NULL)
+	{
+		if (p1->num == n)
+		{
+			cout << "请输入修改的学号:" << endl;
+			cin >> p1->num;
+			cout << "请输入修改的姓名:" << endl;
+			cin >> p1->name;
+			cout << "请输入修改的性别:" << endl;
+			cin >> p1->sex;
+			cout << "请输入修改的专业:" << endl;
+			cin >> p1->major;
+			cout << "请输入修改的地址:" << endl;
+			cin >> p1->address;
+			cout << "请输入修改的英语成绩:" << endl;
+			cin >> p1->englishscore;
+			cout << "请输入修改的出生日期:" << endl;
+			cout << "请输入修改的年:" << endl;
+			cin >> p1->year;
+			cout << "请输入修改的月:" << endl;
+			cin >> p1->month;
+			cout << "请输入修改的日:" << endl;
+			cin >> p1->date;
+		}
+		p1 = p1->next;
+	}
+	cout << "修改成功!!" << endl;
+}
+void stu::storestudent(stu* p)//储存学生信息 
+{
+	stu* p1;
+	p1 = p->next;
+	ofstream fs;
+	fs.open("学生信息.txt", ios::out);
+	if (!fs)
+	{
+		cout << "无法打开学生信息。" << endl;
+		exit(0);
+	}
+	while (p1 != NULL)
+	{
+		fs << "学号:"<<p1->num << "\t"<<"姓名:" << p1->name << "\t" <<"性别:"<< p1->sex << "\t" <<"专业:"<< p1->major << "\t" <<"出生日期"<< p1->year << "." << p1->month << "." << p1->date << "\t" <<"地址"<< p1->address << "\t"<<"英语成绩" << p1->englishscore << endl;
+		p1 = p1->next;
+	}
+	cout << "存入学生信息已完成。" << endl;
+	fs.close();
+	system("pause");
+}
+void stu::readstudent()//读取学生信息 
+{
+	ifstream ifs;
+	ifs.open("学生信息.txt",ios::in);
+	if(!ifs.is_open())
+	{
+		cout<<"文件打开失败"<<endl;
+		return;
+	}
+	string buf;
+	while(getline(ifs,buf))
+	{
+		cout<<buf<<endl;
+	}
+	ifs.close();
+	system("pause");
+} 
+/*else if (type == c)                                          //按年龄统计 
+	{
+		int age;
+		cout << "请输入您要统计的年龄：\n";
+		cin >> age;
+		if (p1 == NULL)
+		{
+			cout << "无学生信息！\n";
+		}
+		else
+		{
+			int total = 0;
+			time_t tt = time(NULL);                                        //这句返回的只是一个时间cuo
+			tm* t = localtime(&tt);
+			while (p1 != NULL)
+			{
+				int agenow = 0;
+				if (t->tm_year + 1900 >= p1->year && t->tm_mon + 1 >= p1->month && t->tm_mday >= p1->date)
+				{
+					agenow = t->tm_year + 1900 - p1->year;
+				}
+				else
+				{
+					agenow = t->tm_year + 1900 - p1->year - 1;
+				}
+				if (agenow == age)
+				{
+					total++;
+				}
+				p1 = p1->next;
+			}
+			cout << "年龄为" << age << "的学生人数为：\t" << total << endl;
+		}
+	}
+	system("pause");
+}*/
